b3d7542cfb70e992d96c1683ba3290abc06b92bb
SamuelFoc <samo.sipikal@gmail.com>
2023-02-15 16:15:12 +0100

    Add syntax highlighting to Articles

--- components/Article.jsx
+++ components/Article.jsx
@@ -11,12 +11,18 @@ export default function Article(props) {
       {/* Two links below are very important for styling of MD and LaTex! */}
       <link
         rel="stylesheet"
+        href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/atom-one-dark.min.css"
+      />
+      <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js"></script>
+      <link
+        rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css"
       ></link>
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/markdown-it-texmath/css/texmath.min.css"
       ></link>
+      <script>hljs.initHighlighting();</script>
     </div>
   );
 }
