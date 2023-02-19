e9c1f6a2b98aa282d3e5cf29307243cc8be11373
SamuelFoc <samo.sipikal@gmail.com>
2023-02-19 18:49:05 +0100

    Adjust Responsivity

--- components/Article.jsx
+++ components/Article.jsx
@@ -2,27 +2,18 @@ import styles from "./Article.module.css";
 
 export default function Article(props) {
   return (
-    <div className={styles.mainContainer}>
+    <div>
       <h2 className={styles.title}>{props.title}</h2>
       <div
-        className={styles.articleContainer}
+        className={styles.container}
         dangerouslySetInnerHTML={{ __html: props.article }}
       ></div>
       {/* Two links below are very important for styling of MD and LaTex! */}
-      <link
-        rel="stylesheet"
-        href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/atom-one-dark.min.css"
-      />
-      <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js"></script>
+      <link rel="stylesheet" href="/Highlighter.css" />
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css"
       ></link>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/markdown-it-texmath/css/texmath.min.css"
-      ></link>
-      <script>hljs.initHighlighting();</script>
     </div>
   );
 }
