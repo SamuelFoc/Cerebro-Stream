c6aa60b1d3b98b2caa77c480931f043836ebb311
SamuelFoc <samo.sipikal@gmail.com>
2023-02-22 15:08:12 +0100

    Add Dark/Light mode toggle

---
+++ components/ThemeButton.jsx
@@ -0,0 +1,31 @@
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import styles from "./ThemeButton.module.css";
+
+export default function ThemeButton() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div className={styles.container}>
+      {theme === "dark" ? (
+        <button className={styles.button} onClick={() => setTheme("light")}>
+          <FontAwesomeIcon icon={faSun} />
+        </button>
+      ) : (
+        <button className={styles.button} onClick={() => setTheme("dark")}>
+          <FontAwesomeIcon icon={faMoon} />
+        </button>
+      )}
+    </div>
+  );
+}
