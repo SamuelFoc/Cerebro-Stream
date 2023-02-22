38db0a54d3f0448b08dc4b64032776c682efeb81
SamuelFoc <samo.sipikal@gmail.com>
2023-02-22 12:29:34 +0100

    Remove from this branch

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,6 @@
 import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 import styles from "./Layout.module.css";
-import LabButton from "./LabButton";
 
 export default function Layout({ children }) {
   return (
@@ -12,7 +11,6 @@ export default function Layout({ children }) {
         items={["BackEnd", "FrontEnd"]}
       />
       <main className={styles.mainContainer}>{children}</main>
-      <LabButton />
       <Footer />
     </div>
   );
